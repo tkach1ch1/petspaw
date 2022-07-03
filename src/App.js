72a956f2a9dce17db981dd03185327e9941870e8
Bogdan Tkach <tkachbogdan0205@gmail.com>
2022-07-03 12:33:41 +0300

    feat: main pages routes setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import { Typography } from '@mui/material';
 import { theme } from './styles/theme';
 import { ThemeProvider } from '@mui/system';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from 'react-router-dom';
+import Home from './pages/Home';
+import Breeds from './pages/Breeds';
+import Gallery from './pages/Gallery';
+import Voting from './pages/Voting';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Typography>MacPaw_Test</Typography>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/breeds' element={<Breeds />} />
+          <Route path='/gallery' element={<Gallery />} />
+          <Route path='/voting' element={<Voting />} />
+          <Route path='*' element={<Navigate to='/' />} />
+        </Routes>
+      </Router>
     </ThemeProvider>
   );
 }
